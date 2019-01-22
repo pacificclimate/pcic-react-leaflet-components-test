87e999f9fd7938751820272ffaeb98993dfe2ea2
Rod Glover <rglover@uvic.ca>
2019-01-22 14:16:49 -0800

    Add simple test of pcic-react-leaflet-components - all good

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { TextInput } from 'pcic-react-leaflet-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <div style={{ width: 640, margin: "15px auto" }}>
+      <h1>Hello React</h1>
+      <TextInput label="Email Address" placeholder="name@example.com" />
+    </div>
+  );
+};
